4484d09fab53337ff34466b3596ae6e3aec78b15
Ulkar Aghayeva <ulkarragh@code.edu.az>
2024-11-24 01:18:58 +0400

    update

--- middlewares/protectRoute.js
+++ middlewares/protectRoute.js
@@ -5,18 +5,16 @@ export const protectRoute = (request, response, next) => {
   
     const token = request.cookies.jwt || request.headers.authorization?.split(" ")[1];
 
-
     if (!token) {
         return response.status(401).send({message: "No token, authorization denied"})
     }
 
     try {
         const { id } = jwt.verify(token, process.env.JWT_SECRET)
-        
         request.id = id
-
         next()
     } catch (err) {
         return response.status(401).send({message: "Token is not valid"})
     }
 }
+
